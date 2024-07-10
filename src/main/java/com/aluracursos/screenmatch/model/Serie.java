ea406bac6214f2b1473baf6e1d4b652b3e699800
Alejandro Salas <jasnsalas@outlook.com>
2024-07-10 01:31:06 -0400

    primer commit

---
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -0,0 +1,125 @@
+package com.aluracursos.screenmatch.model;
+
+import com.aluracursos.screenmatch.service.ConsultaChatGPT;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+@Entity
+@Table(name = "series")
+public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(unique = true)
+    private String titulo;
+    private Integer totalTemporadas;
+    private Double evaluacion;
+    private String poster;
+    @Enumerated(EnumType.STRING)
+    private Categoria genero;
+    private String actores;
+    private String sinopsis;
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Episodio> episodios;
+
+    public Serie() {}
+
+    public Serie(DatosSerie datoSerie) {
+        this.titulo = datoSerie.titulo();
+        this.totalTemporadas = datoSerie.totalTemporadas();
+        this.evaluacion = OptionalDouble.of(Double.parseDouble(datoSerie.evaluacion())).orElse(0);
+        this.poster = datoSerie.poster();
+        this.genero = Categoria.fromString(datoSerie.genero().split(",")[0].trim());
+        this.actores = datoSerie.actores();
+        this.sinopsis = datoSerie.sinopsis();
+    }
+
+    @Override
+    public String toString() {
+        return "genero=" + genero +
+                ", titulo='" + titulo + '\'' +
+                ", totalTemporadas=" + totalTemporadas +
+                ", evaluacion=" + evaluacion +
+                ", poster='" + poster + '\'' +
+                ", actores='" + actores + '\'' +
+                ", sinopsis='" + sinopsis + '\'' +
+                ", episodios='" + episodios + '\'';
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
+        this.episodios = episodios;
+    }
+}
